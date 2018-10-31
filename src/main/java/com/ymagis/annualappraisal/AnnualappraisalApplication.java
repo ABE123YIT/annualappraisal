7234cb9fd40e6c4095b9124bcbd985d0e109ca81
ABE <abenabdellouahad-nyg@ymagisgroup.com>
2018-10-31 14:59:23 +0000

    first commit

---
+++ src/main/java/com/ymagis/annualappraisal/AnnualappraisalApplication.java
@@ -0,0 +1,12 @@
+package com.ymagis.annualappraisal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnualappraisalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnualappraisalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ymagis/annualappraisal/AnnualappraisalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ymagis.annualappraisal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnnualappraisalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
